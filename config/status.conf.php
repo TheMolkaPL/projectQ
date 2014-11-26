b78900878e93aa8a52538ee78be4ef3b68b43d62
SlimaK <em.slimak@gmail.com>
2014-11-27 00:14:09 +0100

    Błąd logiczny w komentarzu do konfiguracji.

--- config/status.conf.php
+++ config/status.conf.php
@@ -17,5 +17,5 @@ define("STATUS_IP", "127.0.0.1");
 // Port dla serwera Minecraft, zwykle można zostawić domyślny.
 define("STATUS_PORT", "25565");
 
-// Wpisz tutaj wersję serwera. Bug: Serwer testowy 1.7.10 działa po wpisaniu tutaj 1.6.*, a nie 1.7.*.
+// Wpisz tutaj wersję serwera. Bug: Dla serwera testowego 1.7.10 działa po wpisaniu tutaj 1.6.*, a nie 1.7.*.
 define("STATUS_VER", "1.6.*");
