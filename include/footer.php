003285ef4c9a3fbf70eb6cde7c89bcd9df62b901
SlimaKCoder <em.slimak@gmail.com>
2014-11-27 14:55:48 +0100

    Dodanie pliku odpowiedzialnego za stopkę.

---
+++ include/footer.php
@@ -0,0 +1,19 @@
+<?php if (__FILE__ == $_SERVER['DOCUMENT_ROOT'].$_SERVER['PHP_SELF']) die();
+/**
+ * Stopka dla wszystkich stron.
+ * @version     1.0
+ * @package     projectQ
+ * @author      SlimaK <em.slimak@gmail.com>
+ * @license     https://creativecommons.org/licenses/by-nd/4.0/legalcode Creative Commons Attribution-NoDerivatives 4.0 International License
+ * @copyright   Copyright (c) 2014, SlimaK
+ */
+?>
+    <!-- Usunięcie tej stopki jest pogwałceniem praw autorskich. Uszanuj moją pracę. -->
+    <!-- Nie wolno Ci usuwać danych o autorze z żadnego miejsca w kodzie strony. -->
+    <footer style="position: fixed; right: 15px; bottom: 0px; margin: auto;">
+        <h6><a target="_blank" href="https://github.com/SlimaKCoder/projectQ/">Copyright &copy; 2014, SlimaK</a><h6>
+    </footer>
+</body>
+</html>
+
+
